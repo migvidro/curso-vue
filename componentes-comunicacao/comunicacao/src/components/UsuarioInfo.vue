ff6269ce2287ea884eb2cefe831de40da589cb84
Miguel Vidigal Rodrigues <40529409+migvidro@users.noreply.github.com>
2021-02-15 11:49:39 -0300

    Starting project on git
    Uploading existing files

---
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverteNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+    </div>
+</template>
+
+<script>
+// import barramento from '@/barramento'
+
+export default {
+    props: {
+        nome: {
+            type: String,
+            default: 'Anônimo'
+            // required: true
+            // default: function(){
+            //     return Array(10).fill(0).join(',');
+            // }
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods:{
+        inverteNome(){
+            return this.nome.split('').reverse().join('');
+        },
+        reiniciarNome(){
+            //this.nome = 'Pedro';
+            this.$emit('nomeMudou', 'Pedro');
+        }
+        // created(){
+        //     barramento.$on('idadeMudou', (idade) => {
+        //         this.idade = idade;
+        //     });
+        // }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    
+    <navbar/>
+
+    <name/>
+
+    <info/>
+
+    <footerInfo/>
+
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/template/Navbar.vue';
+import Title from '@/components/template/Title.vue';
+import Content from '@/components/widgets/Content.vue';
+import Footer from '@/components/template/Footer.vue';
+
+export default {
+  components: {
+    navbar: Navbar,
+    name: Title,
+    info: Content,
+    footerInfo: Footer
+  }
+}
+</script>
